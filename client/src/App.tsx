acb64ab5dbc89efe736e16d7f423e9ec5e27aad8
William Ensoll <willensoll@Williams-MacBook-Air.local>
2024-11-25 14:00:17 +0000

    import stopwatch into app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,14 @@
 import './App.css'
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import {Metrics} from "./components/Metrics/Metrics.tsx";
+import {Stopwatch} from "./components/Stopwatch/Stopwatch.tsx";
 
 function App() {
     const queryClient = new QueryClient()
     return (
         <QueryClientProvider client={queryClient}>
             <div className={"container"}>
-                <div className={"leftPanel"}></div>
+                <div className={"leftPanel"}><Stopwatch/></div>
                 <div className={"rightPanel"}><Metrics/></div>
             </div>
         </QueryClientProvider>
